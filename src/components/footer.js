028091000f657383adaf4ccfc6f25d4809e55457
Jasmin Dozier <jasmindozier93@gmail.com>
2019-06-11 22:06:14 -0400

    Setup initial components

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = props => {
+  return (
+    <footer className="page-footer font-small blue">
+      <div className="footer-copyright text-center py-3">
+        © 2019 Copyright:
+        <a href="https://calendar-todo.com"> Calendar-todo.com</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
